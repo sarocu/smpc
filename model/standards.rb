aff62a81b8ea2b2599a0467550f37a9f8188e9b9
Sam Currie <sam.currie@simuwatt.com>
2017-05-28 21:48:21 -0600

    added prototype scaffold method

--- model/standards.rb
+++ model/standards.rb
@@ -13,8 +13,37 @@ class StandardModel
 	# - vintage
 	# - HVAC system type
 	# - conditioned floor area
-	def scaffold(buildingtypes=[{:type=>'largeoffice', :conditionedarea=>100.0}])
+	def scaffold(prototype={:type=>'largeoffice', :vintage=>2017, :climatezone=>'5B'})
+		@ashraeClimateZone = 'ASHRAE 169-2006-' + prototype[:climatezone]
+		set_template(prototype[:vintage])
+		@model.create_prototype_building(prototype[:type], @template, @ashraeClimateZone, @weatherfile)
+	end
 
+	# Set the template instance variable and collect the standard data for it
+	def set_template(vintage=2017)
+		standard = ''
+	    if vintage < 1980
+	      standard = 'DOE Ref Pre-1980'
+	    elsif vintage >= 1980 and @vintage < 2004
+	      standard = 'DOE Ref 1980-2004'
+	    elsif vintage >= 2004 and @vintage < 2007
+	      standard = '90.1-2004'
+	    elsif vintage >= 2007 and @vintage < 2010
+	      standard = '90.1-2007'
+	    elsif vintage >= 2010 and @vintage < 2013
+	      standard = '90.1-2010'
+	    elsif vintage >= 2013
+	      standard = '90.1-2013'
+	    else
+	      standard = 'none'
+	    end
+
+    	@template = standard
+	end
+
+	#
+	def set_weather(weatherpath='./')
+		@weatherfile = weatherpath
 	end
 
 	# Scale the standard geometry for the model to match the floor area given
@@ -47,7 +76,7 @@ class StandardModel
 
 	# Return the associated model
 	def get
-		#
+		return @model
 	end
 
 
